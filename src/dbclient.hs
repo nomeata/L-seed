caaffe14daac6b07759d6e81e8264f45bf5f7456
Joachim Breitner <mail@joachim-breitner.de>
2009-05-17 23:23:00 +0200

    viewer that reads code from the DB

---
+++ src/dbclient.hs
@@ -0,0 +1,15 @@
+import Lseed.Data
+import Lseed.DB
+import Lseed.Grammar.Compile
+import Lseed.Grammar.Parse
+import Lseed.Logic
+import Control.Applicative
+
+getGarden = spread <$> map (either (error.show) compileGrammarFile . parseGrammar "" . dbcCode)
+		   <$> getCodeToRun
+  where spread gs = zipWith (\g p -> Planted ((p + 0.5) / l) g (Stipe () 0 [])) gs [0..]
+	  where l = fromIntegral (length gs)
+
+main = do
+	garden <- getGarden
+	runGarden garden
