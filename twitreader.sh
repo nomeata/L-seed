6d5d7ef320809434fa531163bc561e193dc5677c
Joachim Breitner <mail@joachim-breitner.de>
2009-06-27 15:25:55 +0200

    Better twit output

--- twitreader.sh
+++ twitreader.sh
@@ -1,12 +1,13 @@
 while sleep 10;
 do
 GET "http://search.twitter.com/search.atom?q=%23GPN8&rpp=1" > msg.atom 
-echo -n " Via Twitter: " \
-$(
+echo -n "" \
+$(echo $(
 	xpath -q -e '//author/name/text()' msg.atom  |
-	lynx -dump -stdin|lynx -dump -stdin -nolist
-) \
-": " \
+	lynx -dump -stdin|lynx -dump -stdin -nolist |
+	cut -d\( -f1
+))\
+"@twitter:" \
 $(
 	xpath -q -e '//content/text()' msg.atom  |
 	lynx -dump -stdin|lynx -dump -stdin -nolist
