d0c14a6156c00eeb5208efeedae1fc6a18452fff
Joachim Breitner <mail@joachim-breitner.de>
2009-06-27 11:52:11 +0200

    Disable Cache for generated Images

--- web/php/PlantImages.php
+++ web/php/PlantImages.php
@@ -55,6 +55,11 @@
 			// 1 => Lesehandle, das auf das Child STDOUT verbunden ist
 			// Jedwede Fehlerausgaben werden an /tmp/error-output.txt angefügt
 			header("Content-type: image/png");
+			header( "Expires: Mon, 20 Dec 1998 01:00:00 GMT" );
+			header( "Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT" );
+			header( "Cache-Control: no-cache, must-revalidate" );
+			header( "Pragma: no-cache" );
+
 
 			fwrite($pipes[0], $plant->Code);
 			fclose($pipes[0]);
