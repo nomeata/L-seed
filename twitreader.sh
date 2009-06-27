b8651523c31af266c3fdec0b9049eeae60f4a94d
Joachim Breitner <mail@joachim-breitner.de>
2009-06-27 15:57:58 +0200

    Now leading space in twitreader

--- twitreader.sh
+++ twitreader.sh
@@ -1,7 +1,7 @@
 while sleep 10;
 do
 GET "http://search.twitter.com/search.atom?q=%23GPN8&rpp=1" > msg.atom 
-echo -n "" \
+echo -n \
 $(echo $(
 	xpath -q -e '//author/name/text()' msg.atom  |
 	lynx -dump -stdin|lynx -dump -stdin -nolist |
